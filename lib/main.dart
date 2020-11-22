c2ca865ba7b6c5217ec7ff95fd4401801ae66d9e
Gustavo <gustavo.loureiro@buser.com.br>
2020-11-22 19:25:00 -0300

    sandbox for basic implicit animations

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animations/screens/sandbox.dart';
 import './screens/home.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Ninja Trips',
-      home: Home(),
+      home: Sandbox(),
     );
   }
 }
---
+++ lib/screens/sandbox.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class Sandbox extends StatefulWidget {
+  @override
+  _SandboxState createState() => _SandboxState();
+}
+
+class _SandboxState extends State<Sandbox> {
+  double _opacity = 1;
+  double _margin = 0;
+  double _width = 200;
+  Color _color = Colors.blue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnimatedContainer(
+        margin: EdgeInsets.all(_margin),
+        duration: Duration(seconds: 1),
+        width: _width,
+        color: _color,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: Text('animate margin'),
+              onPressed: () => setState(() => _margin = 50),
+            ),
+            RaisedButton(
+              child: Text('animate color'),
+              onPressed: () => setState(() => _color = Colors.cyanAccent[200]),
+            ),
+            RaisedButton(
+              child: Text('animate width'),
+              onPressed: () => setState(() => _width = 400),
+            ),
+            RaisedButton(
+              child: Text('animate opacity'),
+              onPressed: () => setState(() => _opacity = 0.0),
+            ),
+            AnimatedOpacity(
+              opacity: _opacity,
+              duration: Duration(seconds: 2),
+              child: Text(
+                "Hide me",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
