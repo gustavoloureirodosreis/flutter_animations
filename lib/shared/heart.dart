8bd7ef56f7e5a55a5080bc63c9097fd59b879c6c
Gustavo <gustavo.loureiro@buser.com.br>
2020-11-28 12:24:27 -0300

    @animates heart size

--- lib/shared/heart.dart
+++ lib/shared/heart.dart
@@ -9,6 +9,7 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
   bool isFav = false;
   AnimationController _controller;
   Animation<Color> _colorAnimation;
+  Animation<double> _sizeAnimation;
 
   @override
   void initState() {
@@ -21,6 +22,19 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
 
     _colorAnimation = ColorTween(begin: Colors.grey[400], end: Colors.red)
         .animate(_controller);
+
+    _sizeAnimation = TweenSequence(
+      <TweenSequenceItem<double>>[
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 30, end: 50),
+          weight: 50,
+        ),
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 50, end: 30),
+          weight: 50,
+        ),
+      ]
+    ).animate(_controller);
 
     _controller.addStatusListener((status) {
       if (status == AnimationStatus.completed) {
@@ -52,7 +66,7 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
           icon: Icon(
             Icons.favorite,
             color: _colorAnimation.value,
-            size: 30,
+            size: _sizeAnimation.value,
           ),
           onPressed: () {
             isFav ? _controller.reverse() : _controller.forward();
